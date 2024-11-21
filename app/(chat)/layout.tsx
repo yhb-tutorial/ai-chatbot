d2191c844c532a64b0b521af70d2582261b4ed9d
haibin.yang <dev@ververv.com>
2024-11-21 15:18:31 +0800

    入口去除Auth

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,23 +1,38 @@
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 
-import { AppSidebar } from '@/components/app-sidebar';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import { auth } from '../(auth)/auth';
+import { auth } from "../(auth)/auth";
 
 export const experimental_ppr = true;
+
+// export interface User {
+//   id?: string
+//   name?: string | null
+//   email?: string | null
+//   image?: string | null
+// }
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [session, cookieStore] = await Promise.all([auth(), cookies()]);
-  const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
+  // const [session, cookieStore] = await Promise.all([auth(), cookies()]);
+  // const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
+  const isCollapsed = false;
+  const user = {
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    image: "https://example.com/avatar.png",
+  };
 
   return (
     <SidebarProvider defaultOpen={!isCollapsed}>
-      <AppSidebar user={session?.user} />
+      {/* <AppSidebar user={session?.user} /> */}
+      <AppSidebar user={user} />
       <SidebarInset>{children}</SidebarInset>
     </SidebarProvider>
   );
